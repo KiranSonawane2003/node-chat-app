aeeafec46fe32f5ab8ac5f0b5cdc7963a693f3b0
Suraj Chandgude <surajchandgude0304@gmail.com>
2018-10-12 19:58:56 +0530

    Added Server/ Utils/ messages for dynamic message object

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,8 @@ var socket = io();
                 socket.on('connect', function()  {
                     console.log("Connected to server");
 
-                    // socket.emit('createMessage', {
-                    //     from: "client",
-                    //     message: "CreateMessage event sent from client to server."
-                    // })
-
                     socket.on('newMessage', function(newMessage) {
-                        console.log("newMessage from the Server", newMessage);
+                        console.log("newMessage from the Server\n", newMessage);
                     });
                 }); 
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const http = require("http");
+const {generateMessage} = require("./utils/messages")
 const socketIO = require("socket.io");
 const express = require("express");
 const app = express();
@@ -22,6 +23,13 @@ io.on('connection', (socket) => {
             message: message.message,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     message: message.message,
+        //     createdAt: new Date().getTime()
+        // });
+
     });
     socket.on('disconnect', () => {
         console.log("User was /disconnected");
