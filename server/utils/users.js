1ed21fe5e1a044e04966c9356f9cf8890c807c54
Suraj Chandgude <surajchandgude0304@gmail.com>
2018-10-17 15:36:57 +0530

    Add Users class and updateUserList event

--- server/utils/users.js
+++ server/utils/users.js
@@ -13,20 +13,25 @@
         constructor() {
             this.users = [];
         }
-        addUsers(id, name, room) {
+        addUser (id, name, room) {
             var user = {id, name, room}
-            this.user.push(user);
+            this.users.push(user);
 
             return user;
         }
-        removeUser(id) {
-            var user = this.id;
-            
+        removeUser (id)  {
+             var user = this.getUser(id);
+
+            if(user) {
+                this.users = this.users.filter((user) => user.id !== id );
+            }
+            return user;           
         }
-        getUser(id) {
+        getUser (id) {
+            return this.users.filter((user) => user.id === id )[0]
 
         }
-        getUserList(room) {
+        getUserList (room) {
             var users = this.users.filter((user) => user.room === room );
             var namesArray = users.map((user) => user.name);
             return namesArray
@@ -34,6 +39,7 @@
 
     }
 
+module.exports = {Users};
 //     class Person {
 //         constructor(name, age) {
 //             this.name = 'Else';
